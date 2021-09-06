7ad4910bb73b89ca49d241ba3e8418968ed760d7
Pierre Faniel <13941041+PierreFaniel@users.noreply.github.com>
2021-09-05 21:11:16 -0400

    Cleanup js script

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,11 @@ document.addEventListener('click', function (event) {
 
   var target = event.target
   //Bail if our clicked element doesn't have the class
-  console.info(target.classList + target.parentElement.classList);
   if (!target.classList.contains('heading') && !target.parentElement.classList.contains('heading')) return;
+
   // Get the target content
   var article = target.closest('.article');
   var content = article.querySelector('.content')
-  console.info(content);
-  // var content = document.querySelector(target.hash);
   if (!content) return;
 
   // Prevent default link behavior
@@ -20,12 +18,6 @@ document.addEventListener('click', function (event) {
     content.classList.remove('active');
     return;
   }
-
-  // Get all open accordion content, loop through it, and close it
-  // var accordions = document.querySelectorAll('.accordion-content.active');
-  // for (var i = 0; i < accordions.length; i++) {
-  //   accordions[i].classList.remove('active');
-  // }
 
   // Toggle our content
   content.classList.toggle('active');
