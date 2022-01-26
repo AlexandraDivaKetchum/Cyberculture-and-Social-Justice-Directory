b84b9dd0cea83e17de83547c6b9051b27d58aabb
Pierre Faniel <13941041+PierreFaniel@users.noreply.github.com>
2022-01-26 11:11:28 -0500

    Add tag/keywird autocompletion in search bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,59 @@
-const searchBar = document.getElementById('searchbar');
+const searchBar = document.getElementById('searchBar');
+const keyWords = document.querySelector('meta[name="keywords"]').content;
+const keyWordList = keyWords.split(",");
 
 document.addEventListener('click', function (event) {
   const target = event.target;
-  if (target.classList.contains('heading') || target.parentElement.classList.contains('heading')) {
+  if (target.classList?.contains('heading') || target.parentElement?.classList?.contains('heading')) {
     toggleArticleContent(target);
     return;
   }
 
   if (target.nodeName === 'LABEL') {
     onTagClicked(target);
+  }
+});
+
+
+searchBar.addEventListener('keyup', function (event) {
+  if (event.key === 'Enter' || event.keyCode === 13) {
+    searchTags();
+  }
+});
+
+const autoCompleteJS = new autoComplete({
+  selector: "#searchBar",
+  placeHolder: "Search tags...",
+  data: {
+      src: keyWordList,
+      cache: true,
+  },
+  resultsList: {
+      element: (list, data) => {
+          if (!data.results.length) {
+              // Create "No Results" message element
+              const message = document.createElement("div");
+              // Add class to the created element
+              message.setAttribute("class", "no_result");
+              // Add message text content
+              message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
+              // Append message element to the results list
+              list.prepend(message);
+          }
+      },
+      noResults: true,
+  },
+  resultItem: {
+      highlight: true
+  },
+  events: {
+      input: {
+          selection: (event) => {
+              const selection = event.detail.selection.value;
+              autoCompleteJS.input.value = selection;
+              searchTags();
+          }
+      }
   }
 });
 
